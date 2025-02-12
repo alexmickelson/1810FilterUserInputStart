3095dd5ef51ae59f045caaf83fd60210a4e934af
Alex Mickelson <alexmickelson96@gmail.com>
2025-02-12 08:36:10 -0700

    more simple start

--- site.js
+++ site.js
@@ -2,21 +2,11 @@ import { bookList } from "./books.js";
 
 
 const drawBooks = (books) => {
-  const bookHTML = (book) => `
-    <tr>
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.summary}</td>
-    </tr>
-  `
 
-  const tableBodyElement = document.getElementById("myTableBody")
-  tableBodyElement.innerHTML = "";
-
-  books.forEach(book => {
-    const firstBookHtml = bookHTML(book)
-    tableBodyElement.innerHTML += firstBookHtml;
-  })
+  // generate a table row for each book in the books parameter
+  // set the table row to have data cells for the title, author, and summary
+  // append the new row to the end of the table body
+  // hint: document.getElementById("myTableBody")
 }
 
 drawBooks(bookList);
